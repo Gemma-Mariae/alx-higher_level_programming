767eaea8bb056e7a14e3d09ed3f19d5ac8b63ad0
gemma-mariae <divinechisom1995@gmail.com>
2021-10-07 14:33:35 +0000

    correcting files

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,28 +1,28 @@
-#include "Python.h"
+#!/usr/bin/python3
+'''A module that prints information about Python string objects using
+the Python C API.
+'''
+import ctypes
 
-/**
- * print_python_string - Prints information about Python strings.
- * @p: A PyObject string object.
- */
-void print_python_string(PyObject *p)
-{
-	long int length;
 
-	fflush(stdout);
+lib = ctypes.CDLL('./libPython.so')
+lib.print_python_string.argtypes = [ctypes.py_object]
 
-	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
+# Valid objects
+lib.print_python_string('English: I do not know what you mean')
+lib.print_python_string('Russian: Я не знаю, что вы имеете в виду')
+lib.print_python_string('French: je ne sais pas ce que tu veux dire')
+s = "French: L'huile et l'eau ne peuvent pas être mélangées"
+lib.print_python_string(s)
+lib.print_python_string('Chinese: 我不知道你想表达什么意思')
+lib.print_python_string('Korean: 난 당신이 무슨 뜻인지 모르겠어요')
+lib.print_python_string('Japanese: 意味が分からない')
+lib.print_python_string('Yoruba: Emi ko mọ kini o tumọ si')
+lib.print_python_string('Igbo: Amaghị m ihe ị na -ekwu')
+lib.print_python_string('Kinyarwanda: Sinzi icyo ushaka kuvuga')
+lib.print_python_string('Swahili: Sijui unamaanisha nini')
 
-	length = ((PyASCIIObject *)(p))->length;
-
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
-}
+# Invalid objects
+lib.print_python_string(b'I do not know what you mean')
+lib.print_python_string(45)
+lib.print_python_string(list('I do not know what you mean'))
